2ec97614dc1376c7b001d4213638ae6ce8c8cad3
Daniel Har-Oz <82752992+MikeHaHaya@users.noreply.github.com>
2021-05-06 17:04:55 +0300

    AdminController basic setup

--- projectSpring/src/main/java/app/core/controllers/AdminController.java
+++ projectSpring/src/main/java/app/core/controllers/AdminController.java
@@ -11,4 +11,86 @@ public class AdminController extends ClientController {
 
     @Autowired
     private AdminService service;
+
+    @PostMapping("/add-company")
+    public void addCompany(@RequestHeader String token, @RequestBody Company company) {
+        try {
+            service.addNewCompany(company);
+        } catch (ServiceException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+    }
+
+    @PutMapping("/update-company")
+    public void updateCompany(@RequestHeader String token, @RequestBody Company company) {
+        try {
+            service.updateCompany(company.getId(), company);
+        } catch (ServiceException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/delete-company")
+    public void deleteCompany(@RequestHeader String token, @RequestBody int companyId) {
+        try {
+            service.deleteCompany(companyId);
+        } catch (ServiceException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+    }
+
+    @GetMapping("/get-all-companies")
+    public List<Company> getAllCompanies(@RequestHeader String token) {
+        return service.getAllCompanies();
+    }
+
+    @GetMapping("/get-one-company")
+    public Company getOneCompany(@RequestHeader String token, @RequestBody int companyId) {
+        try {
+            return service.getOneCompany(companyId);
+        } catch (ServiceException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+    }
+
+    @PostMapping("/add-customer")
+    public void addCustomer(@RequestHeader String token, @RequestBody Customer customer) {
+        try {
+            service.addNewCustomer(customer);
+        } catch (ServiceException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+    }
+
+    @PutMapping("/update-customer")
+    public void updateCustomer(@RequestHeader String token, @RequestBody Customer customer) {
+        try {
+            service.updateCustomer(customer.getId(), customer);
+        } catch (ServiceException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/delete-customer")
+    public void deleteCustomer(@RequestHeader String token, @RequestBody int customerId) {
+        try {
+            service.deleteCustomer(customerId);
+        } catch (ServiceException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+    }
+
+    @GetMapping("/get-all-customers")
+    public List getAllCustomers(@RequestHeader String token) {
+        return service.getAllCustomers();
+    }
+
+    @GetMapping("/get-one-customer")
+    public Customer getOneCustomer(@RequestHeader String token, @RequestBody int customerId) {
+        try {
+            return service.getOneCustomer(customerId);
+        } catch (ServiceException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+    }
 }
